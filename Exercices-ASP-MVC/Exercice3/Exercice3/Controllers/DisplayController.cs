d78835173dd399e46de3fc935ba1a96c6ccde919
Elora <eloradeffolin@gmail.com>
2026-02-17 08:48:46 +0100

    Ajout de l'exercice 3

---
+++ Exercices-ASP-MVC/Exercice3/Exercice3/Controllers/DisplayController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TonProjet.Controllers
+{
+    public class DisplayController : Controller
+    {
+        public IActionResult Index()
+        {
+            string prenom = "Toto";
+            int age = 20;
+
+            List<string> hobbies = new List<string>();
+            hobbies.Add("Lecture");
+            hobbies.Add("Sport");
+            hobbies.Add("Musique");
+            hobbies.Add("Jeux vidéo");
+
+            ViewData["Prenom"] = prenom;
+            ViewData["Age"] = age;
+            ViewData["Hobbies"] = hobbies;
+
+            return View();
+        }
+    }
+}
