4f62287a5100c0bd80ad337143f113ead1dc61d2
Elora <eloradeffolin@gmail.com>
2026-02-18 11:07:33 +0100

    Ajout de l'exercice 5

---
+++ Exercices-ASP-MVC/Exercice5/Exercice5/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using Exercice5_Formulaire_Routing.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercice5_Formulaire_Routing.Controllers
+{
+    [Route("Accueil")]
+    public class ProductController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Product model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            TempData["Product"] = model;
+
+            return RedirectToAction("Details");
+        }
+
+        [HttpGet]
+        public IActionResult Details()
+        {
+            int product = TempData["Product"] as Product;
+
+            if (product == null)
+            {
+                return RedirectToAction("Form");
+            }
+
+            return View(product);
+        }
+    }
+}
---
+++ Exercices-ASP-MVC/Exercice5/Exercice5/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo4_Formulaire_Routing.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+
+        public string Name { get; set; }
+        [Required]
+
+        public double Price { get; set; }
+        [Required]
+        [Range(0, 120, ErrorMessage = "Entre 0 et 120 SVP")]
+
+        [StringLength(200)]
+        public string Category { get; set; }
+        [Required]
+
+        public Contact() { }
+        public Contact(int id, string name, double price, string category)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} ({Price}) {Category}";
+        }
+    }
+}
